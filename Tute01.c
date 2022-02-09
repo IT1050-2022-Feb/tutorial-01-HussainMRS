162a8e82e54600df5251cd8b282e0c004abe7acf
IT21361654 <99306542+HussainMRS@users.noreply.github.com>
2022-02-09 08:56:14 +0530

    Update Tute01.c

--- Tute01.c
+++ Tute01.c
@@ -5,21 +5,6 @@
 #include <stdio.h>
 
 int main() {
-  
-   int mark1 , mark2 ;
-   float avg , tot ;
-   
-   printf("ENter the mark for subject1 : ") ;
-   scanf("d" , &mark1 ) ;
-   
-   printf("ENter the mark for subject2 : ") ;
-   scanf("d" , &mark2 ) ;
-   
-   tot = mark1 + mark2 ;
-   
-   avg = tot / 2 ;
-   
-   printf(" average of the std is %.1f " , avg ) ;
    
   return 0;
 }
